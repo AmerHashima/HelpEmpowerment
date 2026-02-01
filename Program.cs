fd6de1334149fb84fb6ca8bb111fc757b36c06c0
Amer Hashima <ameer.hashima@hispeed-erp.com>
2026-02-01 23:54:36 +0200

    Add project files with .gitignore

--- Program.cs
+++ Program.cs
@@ -30,14 +30,14 @@ namespace HelpEmpowermentApi
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            //if (app.Environment.IsDevelopment())
+            //{
                 app.MapOpenApi();
                 
                 // Enable Swagger middleware
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+           // }
 
             app.UseHttpsRedirection();
 
