0d26c3870d3239e77d490fb0486707cf6c623896
Amer Hashima <ameer.hashima@hispeed-erp.com>
2026-06-05 19:01:37 +0300

    data

--- Models/CourseService.cs
+++ Models/CourseService.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using HelpEmpowermentApi.Common;
+using Microsoft.EntityFrameworkCore;
 
 namespace HelpEmpowermentApi.Models
 {
